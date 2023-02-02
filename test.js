43983535d0c385d562ad16592e8803fe2e30cc1d
pushkarhax <pjn7190@gmail.com>
2023-02-02 14:59:21 +0530

    Update test.js

--- test.js
+++ test.js
@@ -5,44 +5,129 @@ const baseURL = 'http://localhost:5000/api';
 
 // Define the test cases
 const testCases = [
-  {
-    name: 'Test case 1: Get all users',
-    url: baseURL + '/users',
-    method: 'GET',
-    expectedStatusCode: 200,
-  },
-  {
-    name: 'Test case 2: Get a specific user',
-    url: baseURL + '/users/:userId',
-    method: 'GET',
-    expectedStatusCode: 200,
-  },
-  {
-    name: 'Test case 3: Create a new user',
-    url: baseURL + '/users',
-    method: 'POST',
-    body: {
-      name: 'John Doe',
-      email: 'john.doe@example.com',
-      password: 'password123',
-    },
-    expectedStatusCode: 201,
-  },
-  {
-    name: 'Test case 4: Update a specific user',
-    url: baseURL + '/users/:userId',
-    method: 'PUT',
-    body: {
-      name: 'Jane Doe',
-    },
-    expectedStatusCode: 200,
-  },
-  {
-    name: 'Test case 5: Delete a specific user',
-    url: baseURL + '/users/:userId',
-    method: 'DELETE',
-    expectedStatusCode: 204,
-  },
+...
+{
+name: 'Test case 1: Sign up a new user',
+url: baseURL + '/signup',
+method: 'POST',
+body: {
+name: 'John Doe',
+email: 'john.doe@example.com',
+password: 'password123',
+},
+expectedStatusCode: 201,
+},
+{
+name: 'Test case 2: Log in an existing user',
+url: baseURL + '/login',
+method: 'POST',
+body: {
+email: 'john.doe@example.com',
+password: 'password123',
+},
+expectedStatusCode: 200,
+},
+{
+name: 'Test case 3: Get all ride data',
+url: baseURL + '/rides-data',
+method: 'GET',
+expectedStatusCode: 200,
+},
+{
+name: 'Test case 4: Offer a ride',
+url: baseURL + '/offers-ride',
+method: 'POST',
+body: {
+origin: 'San Francisco',
+destination: 'Los Angeles',
+departureTime: '2022-07-01T08:00:00.000Z',
+},
+expectedStatusCode: 201,
+},
+{
+name: 'Test case 5: Rate a ride',
+url: baseURL + '/rate',
+method: 'POST',
+body: {
+rideId: 'ride1',
+rating: 4,
+},
+expectedStatusCode: 200,
+},
+{
+name: 'Test case 6: Get location information',
+url: baseURL + '/location?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA',
+method: 'GET',
+expectedStatusCode: 200,
+},
+{
+name: 'Test case 7: Get ride status',
+url: baseURL + '/ride-status?rideId=ride1',
+method: 'GET',
+expectedStatusCode: 200,
+},
+{
+name: 'Test case 8: End a ride',
+url: baseURL + '/end-ride',
+method: 'PUT',
+body: {
+rideId: 'ride1',
+},
+expectedStatusCode: 200,
+},
+{
+name: 'Test case 9: Request a ride',
+url: baseURL + '/request-ride',
+method: 'POST',
+body: {
+origin: 'San Francisco',
+destination: 'Los Angeles',
+},
+expectedStatusCode: 201,
+},
+{
+name: 'Test case 10: Accept a ride request',
+url: baseURL + '/accept-ride',
+method: 'PUT',
+body: {
+rideRequestId: 'request1',
+},
+expectedStatusCode: 200,
+},
+{
+name: 'Test case 11: Update ride location',
+url: baseURL + '/update-location',
+method: 'PUT',
+body: {
+rideId: 'ride1',
+location: {
+lat: 37.7749,
+lng: -122.4194,
+},
+},
+expectedStatusCode: 200,
+},
+{
+name: 'Test case 12: Process payment for a ride',
+url: baseURL + '/process-payment',
+method: 'POST',
+body: {
+rideId: 'ride1',
+paymentMethod: 'Credit Card',
+},
+expectedStatusCode: 200,
+},
+{
+name: 'Test case 13: Send a notification',
+url: baseURL + '/send-notification',
+method: 'POST',
+body: {
+userId: 'user1',
+notificationType: 'Ride Request',
+message: 'You have a new ride request!',
+},
+expectedStatusCode: 200,
+},
 ];
 
 // Run the tests
