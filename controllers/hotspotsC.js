e8c029b024f7d411c223a212420cbf417f838102
mranonymod <64246578+mranonymod@users.noreply.github.com>
2023-02-18 17:45:41 +0530

    hotspots functionality

--- controllers/hotspotsC.js
+++ controllers/hotspotsC.js
@@ -1,37 +1,52 @@
-const {Rider, Driver, Ride , Hotspots} = require('../models/Schemas');
+const { Rider, Driver, Ride, Hotspots } = require("../models/Schemas");
 
 exports.addHotspot = async (req, res, next) => {
-    try {
-      const { hotspot} = req.body;
-      const hotspots = new Hotspots({
-        name : hotspot.name,
-        location : hotspot.locaion
-      })
-      await hotspots.save()
-      res.send({});
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  };
-  
-  exports.fetchHotspot = async (req, res, next) => {
-    try {
-      const { location} = req.body;
-      const hotspots = await Hotspots.find({}).where(location).within(500)
-      res.send({hotspots});
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  };
+  console.log("add");
+  console.log(req.body);
+  try {
+    const hotspot = req.body;
+    const hotspots = new Hotspots({
+      name: hotspot.name,
+      location: hotspot.location,
+    });
+    await hotspots.save();
+    res.send({ hotspots });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
 
-  exports.voteHotspot = async (req, res, next) => {
-    try {
-      const { hotspotId} = req.body;
-      const hotspots = await Hotspots.findById(hotspotId);
-      hotspots.votes = hotspots.votes + 1
-      await hotspots.save()
-      res.send({hotspots});
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  };
+exports.fetchHotspot = async (req, res, next) => {
+  console.log("fetch");
+  console.log(req.body);
+  try {
+    const { coordinates } = req.body;
+    const hotspots = await Hotspots.find({
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: coordinates,
+          },
+          $maxDistance: 1000,
+        },
+      },
+    });
+    console.log(hotspots);
+    res.send({ hotspots });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+exports.voteHotspot = async (req, res, next) => {
+  try {
+    const { hotspotId } = req.body;
+    const hotspots = await Hotspots.findById(hotspotId);
+    hotspots.votes = hotspots.votes + 1;
+    await hotspots.save();
+    res.send({ hotspots });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
--- routes/hotspot.js
+++ routes/hotspot.js
@@ -1,10 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/hotspotC');
+const controller = require("../controllers/hotspotsC");
 
-
-router.post('/fetch', controller.fetchHotspot);
-router.post('/add', controller.addHostspot);
-router.post('/update', controller.voteHotspot);
+router.post("/fetch", controller.fetchHotspot);
+router.post("/add", controller.addHotspot);
+router.post("/update", controller.voteHotspot);
 
 module.exports = router;
