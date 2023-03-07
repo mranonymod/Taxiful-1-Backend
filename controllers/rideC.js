297d79c77bfc05f27e93b19b49fc2f6bbb17c300
mranonymod <64246578+mranonymod@users.noreply.github.com>
2023-03-07 22:59:49 +0530

    new ride interactions

--- controllers/rideC.js
+++ controllers/rideC.js
@@ -1,23 +1,56 @@
-const {Rider, Driver, Ride } = require('../models/Schemas');
+const { Rider, Driver, Ride } = require("../models/Schemas");
 
 exports.rideStatus = async (req, res, next) => {
-    try {
-      const { rideId } = req.params;
-      const ride = await Ride.findById(rideId);
-      res.send({ status: ride.status });
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  };
-  
-  exports.endRide = async (req, res, next) => {
-    try {
-      const { rideId } = req.params;
-      const ride = await Ride.findById(rideId);
-      ride.status = 'completed';
-      await ride.save();
-      res.send({ status: 'completed' });
-    } catch (error) {
-      res.status(400).send(error);
-    }
-  };
+  try {
+    const { rideId } = req.params;
+    const ride = await Ride.findById(rideId);
+    res.send({ status: ride.status });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+exports.endRide = async (req, res, next) => {
+  try {
+    const { rideId } = req.params;
+    const ride = await Ride.findById(rideId);
+    ride.status = "completed";
+    await ride.save();
+    res.send({ status: "completed" });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+exports.rideDetails = async (req, res, next) => {
+  try {
+    const { rideId } = req.params;
+    const ride = await Ride.findById(rideId);
+    res.send(ride);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+exports.userRides = async (req, res, next) => {
+  console.log("user rides fetch");
+  //console.log(req.body);
+  try {
+    const { riderId } = req.body;
+    const rides = await Ride.find({ rider: riderId });
+    res.send(rides);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+exports.endRide = async (req, res, next) => {
+  try {
+    const { rideId } = req.params;
+    const ride = await Ride.findById(rideId);
+    ride.status = "completed";
+    await ride.save();
+    res.send({ status: "completed" });
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
--- routes/ride.js
+++ routes/ride.js
@@ -1,8 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const controller = require('../controllers/rideC');
+const controller = require("../controllers/rideC");
 // Ride tracking
-router.get('/ride-status', controller.rideStatus);
-router.post('/end-ride', controller.endRide);
+router.get("/ride-status", controller.rideStatus);
+router.post("/ride-details", controller.rideDetails);
+router.post("/end-ride", controller.endRide);
+router.post("/user-rides", controller.userRides);
 //router.post('/process-payment', controller.processPayment);
 module.exports = router;
