58a53033a68de43d4b53492309030b743064fa2d
mranonymod <64246578+mranonymod@users.noreply.github.com>
2023-03-16 11:19:03 +0530

    Update rideC.js

--- controllers/rideC.js
+++ controllers/rideC.js
@@ -43,15 +43,3 @@ exports.userRides = async (req, res, next) => {
     res.status(400).send(error);
   }
 };
-
-exports.endRide = async (req, res, next) => {
-  try {
-    const { rideId } = req.params;
-    const ride = await Ride.findById(rideId);
-    ride.status = "completed";
-    await ride.save();
-    res.send({ status: "completed" });
-  } catch (error) {
-    res.status(400).send(error);
-  }
-};
