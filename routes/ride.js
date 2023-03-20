dbc231dec9fed611c3cde629ed1849d2a49dce9a
mranonymod <64246578+mranonymod@users.noreply.github.com>
2023-03-20 17:51:24 +0530

    Update ride.js

--- routes/ride.js
+++ routes/ride.js
@@ -6,5 +6,6 @@ router.get("/ride-status", controller.rideStatus);
 router.post("/ride-details", controller.rideDetails);
 router.post("/end-ride", controller.endRide);
 router.post("/user-rides", controller.userRides);
+router.post("/fetch-driver-rides", controller.driverRides);
 //router.post('/process-payment', controller.processPayment);
 module.exports = router;
